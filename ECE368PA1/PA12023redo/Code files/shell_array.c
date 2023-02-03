c1cf57330361e96c98d893ff6cd2006a96ae44c6
Rohith Kothandaraman <kothandaramanrohith@purdue.edu>
2023-02-03 15:57:19 -0500

    fixing PA1 initial commit

---
+++ ECE368PA1/PA12023redo/Code files/shell_array.c
@@ -0,0 +1,122 @@
+#include "shell_list.h"
+#include "shell_array.h"
+#include "list_of_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+long *Array_Load_From_File(char *filename, int *size)
+{
+
+    FILE *fptr;
+    fptr = fopen(filename, "rb");
+    if (fptr == NULL)
+    {
+        *size = 0;
+        return NULL;
+    }
+    else
+    {
+        int numLong = 0;
+        long value;
+        while (fread(&value, sizeof(long), 1, fptr))        
+        {
+            numLong++;
+        }
+        if (numLong == 0)
+        {
+            *size = 0;
+            return NULL;
+        }
+
+        *size = numLong;
+        long *arr = malloc(sizeof(long) * numLong);
+        fseek(fptr, 0, SEEK_SET);
+        int ind = 0;
+        while (ind < numLong)
+        {
+            while (fread(&arr[ind], sizeof(long), 1, fptr))
+            {
+            ind++;    
+            }
+            
+        }
+        fclose(fptr);
+        return arr;
+    }
+}
+
+int Array_Save_To_File(char *filename, long *array, int size)
+{
+
+    FILE *fptr = fopen(filename, "wb");
+    int saveCount = 0;
+    if (fptr == NULL || size == 0 || array == NULL)
+    {
+        fclose(fptr);
+        return 0;
+    }
+    else
+    {
+        saveCount = fwrite(array, sizeof(long), size, fptr);
+    }
+    fclose(fptr);
+    return saveCount;
+}
+
+int knuthFunc(int p)
+{
+    if (p < 1)
+        return 0;
+
+    if (p == 1)
+        return 1;
+    return 3 * kunthFunc(p - 1) + 1;
+}
+
+void Array_Shellsort(long *array, int size, long *n_comp){
+     int b=0;
+     int p;
+     int gap[size];
+    for(p=1;p<size;p++){
+        int v=kunthFunc(p);
+        if(v>size){           
+
+            break;
+        }
+        gap[p-1] =v;
+    }
+   // printf("value %d",gap);
+    int index = 0;
+    int k =0;
+    int j =0;
+    int i =0;
+
+    for(index = p-1; index>=0; index--){
+        k = gap[index];
+        for(j=k; j<p-1; j++){
+            int tempr = array[j];
+            int i=j;
+                while(i>= k&& array[i-k] >tempr){
+                array[i] = array[i-k];
+                i = i-k;
+                *n_comp= *n_comp+1;
+            }
+            array[i] = tempr;
+        }
+    }
+
+/*
+    int outer=0;
+    int inner=0;
+for(outer=0;outer<gap;outer++){
+
+        for(inner=gap;inner<size;inner++){
+            if(cmp(array[outer],array[outer+inner])
+        }
+    
+*/
+}
